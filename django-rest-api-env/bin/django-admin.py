8ef07bb9f5be5831a937b0450f73fe60bae6c33b
ashdeepupadhyay <ashdeepupadhyay@gmail.com>
2020-02-25 15:26:10 +0530

    basic setup ,environment setup,serializer,ModelSerializer

---
+++ django-rest-api-env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/ashdeep/ashdeeplearn/projects/python_django/django-rest-api/django-rest-api-env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
